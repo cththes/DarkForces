09dd63b6f3ff44c8ff8b600d0764c285fb73a809
cth <cththes@gmail.com>
2022-07-11 10:24:44 +0300

    10 redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import './App.css';
-import { store } from "./redux/store"
+
 import PeopleContainer from './components/People/PeopleContainer';
 
 function App() {
   return (
 
     <div className="App">
-      <Provider store={store}>
-        <PeopleContainer />
-      </Provider>
+      <PeopleContainer />
     </div>
   );
 }
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,44 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import peopleReducer from "./people-reducer";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
 
 const rootReducer = combineReducers({
   peopleReducer: peopleReducer,
 });
 
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE,
+          PAUSE,
+          PERSIST,
+          PURGE,
+          REGISTER]
+      }
+    })
+})
+
 type RootReducerType = typeof rootReducer
 export type AppStateType = ReturnType<RootReducerType>
 
-export const store = configureStore({
-  reducer: rootReducer,
-});
+export const persistor = persistStore(store)
+export default store
